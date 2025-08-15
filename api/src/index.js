810d502c5646d677df82de5d7ec51b93cf76f24c
CARLOS ÉVERTON ALVES MANGUEIRA <CARLOSEVERTON1995@GMAIL.COM>
2025-08-15 00:56:48 -0300

    i

--- api/src/index.js
+++ api/src/index.js
@@ -13,6 +13,9 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads")); // para servir imagens estáticas
 app.use(express.urlencoded({ extended: true }));
 app.use(router); // todas as rotas registradas aqui
+app.get('/healthz', (req, res) => {
+  res.status(200).send('OK');
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
